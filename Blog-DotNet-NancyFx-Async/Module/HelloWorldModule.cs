e2d1887e4612d2b9da0a99bf67befc411a69b445
Serrano Jorge <jsp@spain.conseur.org>
2017-07-25 17:15:23 +0200

    .NET Framework API REST demo with NancyFx and async.

---
+++ Blog-DotNet-NancyFx-Async/Module/HelloWorldModule.cs
@@ -0,0 +1,27 @@
+﻿namespace Blog_DotNet_NancyFx_Async.Module
+{
+
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class HelloWorldModule : Nancy.NancyModule
+    {
+
+        public HelloWorldModule()
+        {
+            Get["/", runAsync: true] = async (parameters, cancellationToken) =>
+            {
+                return await GetHelloWorld(cancellationToken);
+            };
+        }
+
+        private async Task<string> GetHelloWorld(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return await Task.FromResult("Hello World!");
+        }
+
+    }
+
+}
